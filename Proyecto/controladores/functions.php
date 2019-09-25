03347e78d92dc4e4841c022f921ad5eaedb582c3
JuanStromanIlz <juanstroman@gmail.com>
2019-09-25 01:37:26 -0300

    ya se guarda el registro en json

---
+++ Proyecto/controladores/functions.php
@@ -0,0 +1,66 @@
+<?php
+function validar($datos, $imagen){
+    $errores = [];
+    $userName = trim($datos['userName']);
+    if(empty($userName )){
+        $errores['userName']="El campo nombre no lo puede dejar en blanco";
+    }
+    $lastName = trim($datos['lastName']);
+    if(empty($lastName )){
+        $errores['lastName']="El campo apellido no lo puede dejar en blanco";
+    }
+    $email = trim($datos['email']);
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        $errores['email']="Email inválido";
+    }
+    $password = trim($datos['password']);
+    if(empty($password)){
+        $errores['password']="El password no puede ser blanco";
+    }elseif (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])(?=.*[!?@#$%&])[0-9A-Za-z!?@#$%&]{6,15}$/', $password)) {
+      $errores['password'] = "El password debe tener al menos 6 letras, un numero y un caracter especial";
+    }
+    $passwordRepeat = trim($datos['passwordRepeat']);
+    if($password != $passwordRepeat){
+        $errores['passwordRepeat']="Las contraseñas deben ser iguales";
+    }
+    $foto = $_FILES["avatar"]["name"];
+    $ext = pathinfo($foto, PATHINFO_EXTENSION);
+    if($_FILES ["avatar"]["error"]!=0){
+        $errores["avatar"] = "subi una foto";
+    }
+    elseif ($ext != "png" && $ext != "jpg" && $ext != "jpeg") {
+      echo "subi una imagen como la gente";
+    }
+    return $errores;
+}
+function armarAvatar($imagen){
+    $nombre = $imagen['avatar']['name'];
+    $ext = pathinfo($nombre, PATHINFO_EXTENSION);
+    $archivoOrigen = $imagen['avatar']['tmp_name'];
+    $archivoDestino = dirname(__DIR__);
+    $archivoDestino = $archivoDestino."/profilePics/";
+    $avatar = uniqid();
+    $archivoDestino = $archivoDestino.$avatar.".".$ext;
+    move_uploaded_file($archivoOrigen, $archivoDestino);
+    return $avatar;
+}
+
+function crearRegistro($datos,$imagen){
+    $usuario =[
+        'userName' => $datos['userName'],
+        'lastName' => $datos['lastName'],
+        'email' => $datos['email'],
+        'bornIn' => $datos['bornIn'],
+        'gender' => $datos['gender'],
+        'password'=> password_hash($datos['password'],PASSWORD_DEFAULT),
+        'avatar' => $imagen,
+        'perfil' =>1
+    ];
+    return $usuario;
+}
+function guardarUsuario($usuario){
+    $usuarioJson = json_encode($usuario);
+    file_put_contents('usuario.json',$usuarioJson.PHP_EOL,FILE_APPEND);
+}
+
+ ?>
--- Proyecto/registro.php
+++ Proyecto/registro.php
@@ -1,4 +1,15 @@
-<!DOCTYPE html>
+<?php
+require_once("controladores/functions.php");
+if($_POST && $_FILES){
+  $errores = validar($_POST, $_FILES);
+ if(count($errores)==0){
+   $avatar = armarAvatar($_FILES);
+   $registro = crearRegistro($_POST, $avatar);
+   guardarUsuario($registro);
+   header("location:profile.php");
+ }
+}
+ ?>
 <html lang="en" dir="ltr">
 
 <head>
@@ -12,28 +23,32 @@
 <body>
   <div class="container-fluid py-auto d-flex fondoJuan">
 
-    <form class="col-lg-6 col-md-6 col-sm-12 m-auto py-3 px-5 d-flex flex-wrap  bg-light" action="profile.html" method="POST">
+    <form class="col-lg-6 col-md-6 col-sm-12 m-auto py-3 px-5 d-flex flex-wrap  bg-light" method="POST" enctype="multipart/form-data">
       <div class="w-100 border-bottom mb-3">
         <a href="logIn.php" class="text-decoration-none">
           <h5>Volver</h5>
         </a>
       </div>
+      <?php if(isset($errores)):?>
+              <ul class="alert alert-danger">
+                <?php foreach ($errores as $value) :?>
+                    <li><?=$value;?></li>
+                <?php endforeach;?>
+              </ul>
+            <?php endif;?>
       <div class="form-group w-50 pr-3">
-        <label for="name">Nombre</label>
-        <input type="text" class="form-control" id="name" placeholder="Escribe tu nombre">
+        <label for="userName">Nombre</label>
+        <input type="text" class="form-control" name="userName" id="userName" placeholder="Escribe tu nombre">
       </div>
       <div class="form-group w-50">
         <label for="lastName">Apellido</label>
-        <input type="text" class="form-control" id="lastName" placeholder="Escribe tu apellido">
+        <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Escribe tu apellido">
       </div>
       <div class="form-group w-100">
         <label for="email">Email</label>
         <input type="email" class="form-control" name="email" id="email" placeholder="Escribe tu mail">
       </div>
-      <div class="form-group w-100">
-        <label for="password">Contraseña</label>
-        <input type="password" class="form-control" id="password" placeholder="Introduce tu contraseña">
-      </div>
+
       <div class="form-group w-50 pr-3">
         <label for="bornIn">Nacimiento</label>
         <input type="date" class="form-control pr-3" name="bornIn">
@@ -45,6 +60,17 @@
         <input type="radio" name="gender" value="female"> Mujer
         <input type="radio" name="gender" value="other"> Otro
       </div>
+      <div class="form-group w-100">
+        <label for="password">Contraseña</label>
+        <input type="password" class="form-control" name="password" id="password" placeholder="Al menos 6 letras, un numero y un caracter especial">
+      </div>
+      <div class="form-group">
+        <label for="password">Repetir contraseña</label>
+        <input required name="passwordRepeat" type="password" value= ""class="form-control" name="passwordRepeat" id="passwordRepeat" placeholder="Repetir contraseña">              </div>
+              <div class="form-group">
+                <label for="avatar">Avatar</label>
+                <input required name="avatar" type="file" value= ""class="form-control" id="avatar">
+              </div>
       <button type="submit" class="w-75 m-auto rounded botonJuan py-2">Registrarme</button>
     </form>
   </div>
