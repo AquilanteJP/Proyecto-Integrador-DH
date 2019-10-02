7a698242c917870db72ed14aaeb981a272d4f656
AquilanteJP <aquilantejp@outlook.es>
2019-10-02 17:33:26 -0300

    Incorporacion validacion y mejora login + lo de Lucas

--- Proyecto-Integrador-DH/Proyecto/controladores/functions.php
+++ Proyecto-Integrador-DH/Proyecto/controladores/functions.php
@@ -77,6 +77,7 @@ function crearRegistro($datos,$imagen){  //Usando $_POST como primer parametro y
     ];
     return $usuario;
 }
+
 /* PROBANDO FUNCIONES DE VALIDACION DE LOG IN*/
 function validarLogIn($datos){
     $errores = [];
@@ -95,55 +96,38 @@ function validarLogIn($datos){
     return $errores;
 }
 
-
+function buscarUsuario($email){ //Toma el email guardado en $_POST, consigue el array de usuarios,
+   $arrayUsuarios = abrirBaseDatos();
+   if($arrayUsuarios!==null){
+       foreach ($arrayUsuarios as $usuario =>$value) {
+           if($email === $value["email"]){
+             $usuarioEnDatos=$arrayUsuarios[$usuario];
+             return $usuarioEnDatos;
+           } else {
+             echo "usuario no encontrado";//no se muestra, solo para testear la funcion
+           }
+       }
+   } else {
+      echo "No hay usuarios!";
+      return null;
+   }
+}
 function validarContraseña($passwordPost,$usuarioEnDatos){
-  $usuarios = abrirBaseDatos();
-  if(password_verify($passwordPost,$usuarioEnDatos['password'])){
-      echo "esa contraseña es de este mail";
-      $usuarioSession = $usuarioEnDatos;
-      return $usuarioSession;
-  } else {
-    echo "esa contra no es de este mail";
-  }
-}
-function buscarUsuario($email){
-    $usuarios = abrirBaseDatos();
-    if($usuarios!==null){
-        foreach ($usuarios as $usuario =>$value) {
-            if($email === $value["email"]){
-              $usuarioEnDatos=$usuarios[$usuario];
-                return $usuarioEnDatos;
-              } else {
-              echo "usuario no encontrado";//no se muestra, solo para testear la funcion
-            }
-        }
-    }
-    return null;
+ $usuarios = abrirBaseDatos();
+ if(password_verify($passwordPost,$usuarioEnDatos['password'])){
+     // echo "La contraseña es correcta";
+     $usuarioSession = $usuarioEnDatos;
+     return $usuarioSession;
+ } else {
+   echo "El mail o la contraseña es incorrecta";
+   return null;
+ }
 }
 
-function abrirBaseDatos(){ //gracias profe jajaj
-    if(file_exists("usuarios.json")){
-        $baseDatosJson= file_get_contents("usuarios.json");
-        $baseDatosJson = explode(PHP_EOL,$baseDatosJson);
-        //Aquí saco el ultimo registro, el cual está en blanco
-        array_pop($baseDatosJson);
-        //Aquí recooro el array y creo mi array con todos los usuarios
-        foreach ($baseDatosJson as  $usuarios) {
-            $arrayUsuarios[]= json_decode($usuarios,true);
-        }
-        //Aquí retorno el array de usuarios con todos sus datos
-        return $arrayUsuarios;
-        //var_dump($arrayUsuarios);
-    }else{
-        return null;
-    }
-}
 function guardarUsuario($usuario){  //Pasa $usuario a formato .json y lo guarda en usuarios.json como archivo plano
     $usuarioJson = json_encode($usuario);
     file_put_contents('usuarios.json',$usuarioJson.PHP_EOL,FILE_APPEND);
 }
-
-
 
 function guardarSesion($variable){ // La variable en este caso es $registro, que se crea en registro.php usando la funcion crearRegistro(), y contiene los datos del array $_POST mas la imagen subida. Básicamente, copia todos los datos de $_POST a $_SESSION, para accesibilidad en todas las páginas
   session_start();
