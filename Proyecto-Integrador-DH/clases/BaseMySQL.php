e2ae911bcdba1f80c44039594d1d1405f3dff67f
JuanStromanIlz <juanstroman@gmail.com>
2019-11-02 15:06:27 -0300

    comenzando a preparar la clase base de datos, probando de hacer tablas en mi workbench

--- Proyecto-Integrador-DH/clases/BaseMySQL.php
+++ Proyecto-Integrador-DH/clases/BaseMySQL.php
@@ -1,3 +1,22 @@
 <?php
 
-class BaseMySQL 
+class BaseMySQL {
+
+  protected $usuarios;
+  protected $cursos;
+  protected $toDos;
+  protected $posts;
+
+  public function abrirBaseDatos(){}
+
+  public function agregarUsuario(){}
+
+  public function eliminarUsuario(){}
+
+  public function darPrivilegios(){}
+
+  public function agregarCursoBD(){}
+
+  public function eliminarCursoBD(){}
+
+}
