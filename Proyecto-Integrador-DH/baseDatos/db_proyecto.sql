bea2ce0e6a9088c3bc340d1ac1de94c2ba065503
JuanStromanIlz <juanstroman@gmail.com>
2019-11-09 02:16:48 -0300

    actualizado el archivo .sql

--- Proyecto-Integrador-DH/baseDatos/db_proyecto.sql
+++ Proyecto-Integrador-DH/baseDatos/db_proyecto.sql
@@ -31,7 +31,7 @@ CREATE TABLE `cursos` (
   PRIMARY KEY (`id`),
   KEY `profesor adjunto_idx` (`profesorAdjunto`),
   CONSTRAINT `profesor  adjunto` FOREIGN KEY (`profesorAdjunto`) REFERENCES `usuarios` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci; /*ACA SE CAMBIO utf8mb4_0900_ai_ci POR utf8mb4_unicode_520_ci*/
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,14 +52,12 @@ DROP TABLE IF EXISTS `posts`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(25) NOT NULL,
-  `contenido` varchar(45) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `contenido` varchar(500) NOT NULL,
   `like` varchar(45) DEFAULT NULL,
-  `user id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user Id_idx` (`user id`),
-  CONSTRAINT `post user Id` FOREIGN KEY (`user id`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci; /*ACA SE CAMBIO utf8mb4_0900_ai_ci POR utf8mb4_unicode_520_ci*/
+  `user_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +66,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'El primer post integramente tipeado','hfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrbhfdiuasohvhevñeñrb',NULL,23),(2,'Aguante','messi',NULL,24),(3,'','aguante bokita ',NULL,25);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +85,7 @@ CREATE TABLE `tareas` (
   PRIMARY KEY (`id`),
   KEY `user id_idx` (`user id`),
   CONSTRAINT `tareas user id` FOREIGN KEY (`user id`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci; /*ACA SE CAMBIO utf8mb4_0900_ai_ci POR utf8mb4_unicode_520_ci*/
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,16 +108,18 @@ CREATE TABLE `usuarios` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `tipo_registro` varchar(45) NOT NULL,
   `nombres` varchar(45) NOT NULL,
   `apellidos` varchar(45) NOT NULL,
-  `genero` varchar(45) DEFAULT NULL,
-  `birthdate` varchar(45) DEFAULT NULL,
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
+  `genero` varchar(45) DEFAULT NULL,
+  `birthdate` varchar(45) DEFAULT NULL,
+  `foto_usuario` varchar(45) DEFAULT NULL,
   `contacto` varchar(45) DEFAULT NULL,
   `amigos` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci; /*ACA SE CAMBIO utf8mb4_0900_ai_ci POR utf8mb4_unicode_520_ci*/
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,6 +128,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (23,NULL,NULL,'estudiante','Juan','Stroman','juanstroman@gmail.com','25BAB7F9779AFF3083E7CC41C26CA1D8','male','1998-12-04','juanstroman@gmail.com.jpg',NULL,NULL),(24,NULL,NULL,'estudiante','Bautista','Kees','bautyke0@gmail.com','2A87BFA9B14CD12327D14F1FEFB9D31F','male','2000-01-05','bautyke0@gmail.com.jpg',NULL,NULL),(25,NULL,NULL,'estudiante','dolores','delorenzi','lolidelorenzi2@gmail.com','0BA583D4522BA87BD66DF9EE96E04633','female','1998-03-27','lolidelorenzi2@gmail.com.jpg',NULL,NULL);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -139,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-02 18:39:55
+-- Dump completed on 2019-11-09  2:14:19
