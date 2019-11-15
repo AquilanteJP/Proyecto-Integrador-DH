204abd76b63dd9f7cd865d337f0e5468f3fdf00f
JuanStromanIlz <juanstroman@gmail.com>
2019-11-15 11:17:43 -0300

    editar post arreglado

--- Proyecto-Integrador-DH/editarPost.php
+++ Proyecto-Integrador-DH/editarPost.php
@@ -38,21 +38,25 @@ if (isset($_POST['editAceptado'])) { //Edición de post in-page
   </head>
   <body>
     <div class="container-fluid m-0 p-0 pt-3 d-flex flex-row flex-wrap -all">
-      <div class="row">
+
         <?php include_once("partials/header2.php");?>
-      </div>
-      <div class="col-lg-6 p-0 mt-5 m-3 w-100 bg-light shadow">
-        <form class="p-3 d-flex flex-column" action="" method="post">
-          <h5>Cambia el titulo de tu post aquí:</h5>
-          <input type="text" name="titulo" value="" placeholder="Nuevo título (no más de 30 caracteres)">
-          <hr>
-          <h5>Cambia el contenido de tu post aquí:</h5>
-          <textarea name="contenido" rows="5" cols="" placeholder="Nuevo contenido (no mas de 800 caracteres)"></textarea>
-          <input type="hidden" id="editAceptado" name="editAceptado" value="true">
-          <button type="submit" name="editAceptado" class="btn w-25 mt-2 btn-outline-warning">Editar</button>
-        </form>
-        <button type="button" class="btn w-25 mt-2 btn-outline-danger" name="editCancelado"><a href="inicio.php" class="-editCancelado">Volver</a></button>
-      </div>
+        <div class="mt-4 w-100 d-flex justify-content-center">
+          <div class="col-lg-6 p-0 mt-5 m-3 w-100 bg-light shadow">
+
+            <form class="p-3 d-flex flex-column" action="" method="post">
+              <h5>Cambia el titulo de tu post aquí:</h5>
+              <input type="text" name="titulo" value="" placeholder="Nuevo título (no más de 30 caracteres)">
+              <hr>
+              <h5>Cambia el contenido de tu post aquí:</h5>
+              <textarea name="contenido" rows="5" cols="" placeholder="Nuevo contenido (no mas de 800 caracteres)"></textarea>
+              <input type="hidden" id="editAceptado" name="editAceptado" value="true">
+              <button type="submit" name="editAceptado" class="btn w-25 mt-2 btn-outline-warning">Editar</button>
+              <button type="button" class="btn w-25 mt-2  btn-danger" name="editCancelado"><a href="inicio.php" class="text-decoration-none text-reset">Volver</a></button>
+
+            </form>
+          </div>
+        </div>
+
       <?php include_once('partials/footer.php'); ?>
     </div>
   </body>
